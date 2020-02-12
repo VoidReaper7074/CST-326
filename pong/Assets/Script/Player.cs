82bd3fee9f5cb6458636887f6420f1cdd61138a1
VoidReaper7074 <60455567+VoidReaper7074@users.noreply.github.com>
2020-02-12 09:57:47 -0800

    project

---
+++ pong/Assets/Script/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ePlayer
+{
+    Left,
+    Right,
+}
+
+public class Player : MonoBehaviour
+{
+    public float speed = 15f;
+    public ePlayer player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputspeed = 0f;
+        if (player == ePlayer.Left)
+        {
+            inputspeed = Input.GetAxisRaw("PlayerLeft");
+        }
+        else if(player == ePlayer.Right)
+        {
+            inputspeed = Input.GetAxisRaw("PlayerRight");
+        }
+        transform.position += new Vector3(0f, 0f, inputspeed * speed * Time.deltaTime);
+    }
+}
+
---
+++ pong/Assets/Script/Sound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    public AudioClip colSound;
+    private AudioSource source;
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name != "UpperWall" && collision.gameObject.name != "LowerWall" && collision.gameObject.name != "Floor")
+        {
+            source.PlayOneShot(colSound, 1F);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
